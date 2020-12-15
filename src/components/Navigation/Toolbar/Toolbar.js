fce3c3f48cb3ea605824f50d8b2c0dbd52b294cc
demetrioverba <65371371+demetrioverba@users.noreply.github.com>
2020-12-15 22:20:56 +0200

    Adding Reusable Navigation Items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavgationItems/NavigationItems';
 
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>
-      <ul>
-        ...
-      </ul>
+      <NavigationItems />
     </nav>
   </header>
 
